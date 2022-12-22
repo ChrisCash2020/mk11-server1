6cc41e39f09d420cd072bad1849d8f06ff2665f2
chris <chrisolo2001@gmail.com>
2022-12-22 12:36:14 -0600

    change req type

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const userControllers = require('../controllers/userControllers');
-const router = express.Router();
+const express = require('express')
+const userControllers = require('../controllers/userControllers')
+const router = express.Router()
 
-router.route('/auth/logout').get((req, res) => {
-  res.clearCookie('id');
-  res.status(200).json('cookie id cleared');
-});
+router.route('/auth/logout').post((req, res) => {
+  res.clearCookie('id')
+  res.status(200).json('cookie id cleared')
+})
 
-router.route('/register').post(userControllers.createNewUser);
+router.route('/register').post(userControllers.createNewUser)
 
-router.route('/login').post(userControllers.loginUser);
+router.route('/login').post(userControllers.loginUser)
 
-router.route('/auth').get(userControllers.auth);
+router.route('/auth').get(userControllers.auth)
 
-router.route('/:id').get(userControllers.getUserById);
+router.route('/:id').get(userControllers.getUserById)
 
-module.exports = router;
+module.exports = router
